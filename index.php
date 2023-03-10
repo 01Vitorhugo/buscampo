b3f3b89519f054e52385daac06b6da2bf66c0cdc
Vitor Hugo <vitorhugo0031@outlook.com>
2023-03-10 17:38:55 -0300

    colocando conteudo

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+include('menu.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <title>BusCampo</title>
+</head>
+
+<body>
+
+    <div class="img-gif">
+        <img src="img/gif_futebol.gif" alt="" width="100%" height="100%">
+    </div>
+
+    <div class="frase-1">
+        <h1><i> ALGUMA COISA ESCRITA</i></h1>
+    </div>
+    <div class="topicos-index">
+        <div class="topico-1" id="topicos">
+            <img src="img/cartoon-2.png" alt="">
+        </div>
+        <div class="topico-2" id="topicos">
+            <div class="icons-lupa">
+                <img src="img/icon-lupa.png" alt="">
+            </div>
+            <h2><i>Os melhores lugares</i></h2>
+
+        </div>
+        <div class="topico-3" id="topicos"></div>
+        <div class="topico-4" id="topicos"></div>
+    </div>
+
+</body>
+
+
+<script src="script.js"></script>
+
+</html>
