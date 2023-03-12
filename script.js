4f5e5c4edaedaaa6751d1c7ee9b8051183a07b88
Vitor Hugo <vitorhugo0031@outlook.com>
2023-03-12 16:10:33 -0300

    script

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const sr = ScrollReveal({
     reset: true});
 
     sr.reveal('#topicos', {duration: 1500 });
+
+    const myModalEl = document.getElementById('Modal-body')
+myModalEl.addEventListener('hidden.bs.modal', event => {
+  // do something...
+})
