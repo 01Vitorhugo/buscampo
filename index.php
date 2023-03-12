5e3b07cb415be14dc8a76533599b20fbe23e67d3
Vitor Hugo <vitorhugo0031@outlook.com>
2023-03-12 16:11:38 -0300

    colocando footer no index

--- index.php
+++ index.php
@@ -5,44 +5,58 @@ include('menu.php');
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://unpkg.com/scrollreveal"></script>
-    <link rel="stylesheet" href="css/index.css">
-    <title>BusCampo</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://unpkg.com/scrollreveal"></script>
+  <link rel="stylesheet" href="css/index.css">
+  <title>BusCampo</title>
 </head>
 
 <body>
 
-    <div class="img-gif">
-        <img src="img/gif_futebol.gif" alt="" width="100%" height="100%">
-    </div>
+  <div class="img-gif">
+    <img src="img/gif_futebol.gif" alt="" width="100%" height="100%">
+  </div>
 
-    <div class="frase-1">
-        <h1><i> ALGUMA COISA ESCRITA</i></h1>
+  <div class="frase-1">
+    <h1><i> ALGUMA COISA ESCRITA</i></h1>
+  </div>
+  <div class="topicos-index">
+    <div class="topico-1" id="topicos">
+      <img src="img/cartoon-2.png" alt="">
     </div>
-    <div class="topicos-index">
-        <div class="topico-1" id="topicos">
-            <img src="img/cartoon-2.png" alt="">
-        </div>
-        <div class="topico-2" id="topicos">
-            <div class="icon-campo">
-                <img src="img/icon-campo.png" alt="">
-            </div>
-            <h2><i>Os melhores lugares estão aqui</i></h2>
-            <p>Encontre a quadra que se encaixa em suas preferências.</p>
+    <div class="topico-2" id="topicos">
+      <div class="icon-campo">
+        <img src="img/icon-campo.png" alt="">
+      </div>
+      <h2><i>Os melhores lugares estão aqui</i></h2>
+      <p>Encontre a quadra que se encaixa em suas preferências.</p>
 
-        </div>
-        <div class="topico-3" id="topicos">
-        <div class="icon-lupa">
-                <img src="img/icon-lupa.png" alt="">
-            </div>
-            <h2><i>Os melhores lugares estão aqui</i></h2>
-            <p>Encontre a quadra que se encaixa em suas preferências.</p>
-        </div>
     </div>
+    <div class="topico-3" id="topicos">
+      <div class="icon-lupa">
+        <img src="img/icon-lupa.png" alt="">
+      </div>
+      <h2><i>Os melhores lugares estão aqui</i></h2>
+      <p>Encontre a quadra que se encaixa em suas preferências.</p>
+    </div>
+  </div>
 
+  <footer>
+    <div class="img-footer">
+      <img src="img/logoofc.png" alt="" height="100%" width="100%">
+    </div>
+    <p class="pag-footer">Copyright © 2023 BusCampo</p>
+    <div class="footer-topicos">
+      <p><a href="#">Política de privacidade</a></p>
+      <p><a href="#">Termos de uso</a></p>
+      <p><a href="#">Contato</a></p>
+    </div>
+  </footer>
+
+
+ 
 </body>
 
 
